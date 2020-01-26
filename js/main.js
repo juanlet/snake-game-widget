817dec84bfbf80efa4e2ef44452a0c59abbd4afd
Juan Letamendia <juanletamendia@gmail.com>
2020-01-26 07:53:23 -0300

    added ball

--- js/main.js
+++ js/main.js
@@ -5,10 +5,9 @@ class Game {
     this._ctx.font = "20px Calibri";
     this._width = 500;
     this._height = 500;
-  }
-
-  get ctx() {
-    return this._ctx;
+    this._snake = new Snake();
+    this._food = new Food(this);
+    this.render();
   }
 
   get width() {
@@ -18,33 +17,112 @@ class Game {
   get height() {
     return this._height;
   }
+
+  drawSnake() {
+    const ctx = this._ctx;
+    const snake = this._snake;
+    ctx.save();
+    ctx.fillStyle = snake.color;
+
+    snake.body.map(bp => {
+      ctx.fillRect(bp.x, bp.y, snake.dimensions[0], snake.dimensions[1]);
+    });
+
+    ctx.restore();
+  }
+
+  drawFood() {
+    const food = this._food;
+    const ctx = this._ctx;
+
+    ctx.save();
+    ctx.fillStyle = food.color;
+    ctx.beginPath();
+    ctx.arc(food.x, food.y, 10, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.restore();
+  }
+
+  render() {
+    this.drawSnake();
+    this.drawFood();
+  }
+
+  get ctx() {
+    return this._ctx;
+  }
+
+  generateRandomCoordinate() {
+    const coordinates = this._canvas.getBoundingClientRect();
+    const { top, right, bottom, left } = coordinates;
+
+    const x = Math.floor(Math.random() * right) + left;
+    const y = Math.floor(Math.random() * bottom) + top;
+
+    const roundedX =
+      Math.floor(x / this._snake.dimensions[0]) * this._snake.dimensions[0];
+
+    const roundedY =
+      Math.floor(y / this._snake.dimensions[1]) * this._snake.dimensions[1];
+
+    return [roundedX, roundedY];
+  }
 }
 
 class Snake {
   constructor() {
-    this._snakeBody = [new SnakeBodyPart(20, 20)];
+    this._body = [
+      new SnakeBodyPart(220, 200),
+      new SnakeBodyPart(210, 200),
+      new SnakeBodyPart(200, 200)
+    ];
+    this._color = "green";
+    this._dimensions = [20, 20];
   }
 
-  get bodyWidth() {
-    return this._bodyWidth;
+  //addPart() {}
+
+  get body() {
+    return this._body;
   }
 
-  get bodyHeight() {
-    return this._bodyHeight;
+  get bodyMeasure() {
+    return this._bodyMeasure;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  get dimensions() {
+    return this._dimensions;
   }
 }
 
 class SnakeBodyPart {
-  constructor(width, height) {
-    this._width = width;
-    this._height = height;
+  constructor(x, y) {
+    this._x = x;
+    this._y = y;
+  }
+
+  get x() {
+    return this._x;
+  }
+
+  get y() {
+    return this._y;
   }
 }
 
 class Food {
-  constructor() {
+  constructor(game) {
     this._width = 20;
     this._height = 20;
+    this._color = "orange";
+    const randomCoordinate = game.generateRandomCoordinate();
+    this._x = randomCoordinate[0];
+
+    this._y = randomCoordinate[1];
   }
 
   set width(width) {
@@ -55,12 +133,16 @@ class Food {
     this._height = height;
   }
 
-  get width() {
-    return this._width;
+  get x() {
+    return this._x;
   }
 
-  get height() {
-    return this._height;
+  get y() {
+    return this._y;
+  }
+
+  get color() {
+    return this._color;
   }
 }
 
